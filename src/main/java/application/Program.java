4bccdb7f4dc7c395a1ccf1f525064d6d90c49719
RodrigoMaga <rgn.nunnes@gmail.com>
2026-01-13 16:48:59 -0300

    update implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -5,15 +5,19 @@ import model.dao.SellerDao;
 import model.entitites.Department;
 import model.entitites.Seller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
 
 
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
@@ -36,5 +40,14 @@ public class Program {
         Seller newSeller = new Seller(null, "Friedrich Engels", "friedrich@gmail.com", new Date(), 4000.0, department);
         sellerDao.inset(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(11);
+        seller.setBirthDate(new java.sql.Date(sdf.parse("28/11/1820").getTime()));
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
+
+
+
     }
 }
