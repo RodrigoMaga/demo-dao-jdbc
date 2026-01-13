99bc7b0b3bd58deb7224c434f78a6960f2fd11bf
RodrigoMaga <rgn.nunnes@gmail.com>
2026-01-13 17:05:04 -0300

    delete implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -10,6 +10,7 @@ import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Program {
@@ -18,6 +19,7 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
@@ -38,14 +40,23 @@ public class Program {
 
         System.out.println("\n=== TEST 4: seller insert ===");
         Seller newSeller = new Seller(null, "Friedrich Engels", "friedrich@gmail.com", new Date(), 4000.0, department);
-        sellerDao.inset(newSeller);
-        System.out.println("Inserted! New id = " + newSeller.getId());
+        sellerDao.insert(newSeller);
+//        System.out.println("Inserted! New id = " + newSeller.getId());
 
         System.out.println("\n=== TEST 5: seller update ===");
         seller = sellerDao.findById(11);
         seller.setBirthDate(new java.sql.Date(sdf.parse("28/11/1820").getTime()));
         sellerDao.update(seller);
         System.out.println("Update completed!");
+
+        System.out.println("\n=== TEST 6: seller delete ===");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed!");
+
+        sc.close();
+
 
 
 
